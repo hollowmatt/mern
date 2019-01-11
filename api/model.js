f533ef146e0ca116c9887161a1d6987e2bfd4e36
Matt Holloway <hollowmatt@gmail.com>
2019-01-11 08:30:41 -0700

    created model

---
+++ api/model.js
@@ -0,0 +1,54 @@
+const { connection, Schema } = require('mongoose');
+const crypto = require('crypto');
+
+const UserSchema = new Schema({
+	username: {
+		type: String,
+		minlength: 4,
+		maxlength: 20, 
+		required: [true, 'username is required'],
+		validate: {
+			validator: function (value) {
+				return /^[a-zA-Z]+$/.test(value)
+			},
+			message: '{VALUE} is not a valid username',
+		},
+	},
+	password: String,
+});
+
+serSchema.static('login', async function(usr, pwd) {
+	const hash = crypto.createHash('sha256').update(String(pwd));
+	const user = await this.findOne()
+		.where('username').equals(usr)
+		.where('password').equals(hash.digest('hex'));
+	if(!user) throw new Error('Invalid login');
+	delete user.password;
+	return user;
+});
+
+UserSchema.static('signup', async function(usr, pwd) {
+	if (pwd.length < 6) {
+		throw new Error('Password must be greater than 6 chars');
+	}
+	const hash = crypto.createHash('sha256').update(String(pwd));
+	const user = await this.findOne()
+		.where('username').equals(usr);
+	if(exist) throw new Error('Username already exists');
+	const user = this.create({
+		username: usr,
+		password: hash.digext('hex'),
+	})
+	return user;
+});
+
+UserSchema.method('changePass', async function(pwd) {
+	if (pwd.length < 6) {
+		throw new Error('Password must be greater than 6 chars');
+	}
+	const hash = crypto.createHash('sha256').update(String(pwd));
+	this.password = hash.digest('hex');
+	return this.save();
+});
+
+module.exports = connection.model('User', UserSchema);
