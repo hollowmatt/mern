4ffcb44e00f18fa1bf1414c22279508bbb8cca75
Matt Holloway <hollowmatt@gmail.com>
2019-02-03 09:42:01 -0500

    finished async test

--- async-redux.js
+++ async-redux.js
@@ -42,3 +42,84 @@ const actions = {
 	})
 };
 
+const setValue = (prevState, action) => ({
+	...prevState,
+	value: action.value || null,
+	error: action.error || null,
+	status: action.status || STATUS.RESOLVED
+});
+
+const iniState = {
+	time: {
+		value: null,
+		error: null,
+		status: STATUS.RESOLVED
+	},
+	date: {
+		value: null,
+		error: null,
+		status: STATUS.RESOLVED
+	}
+};
+
+const timeReducer = (state = iniState, action) => {
+	switch (action.type) {
+		case TYPE.FETCH_TIME: 
+			return {
+				...state,
+				time: setValue(state.time, action)
+			}
+		case TYPE.FETCH_DATE:
+			return {
+				...state,
+				date: setValue(state.date, action)
+			}
+		default: 
+			return state
+	}
+};
+
+const allowAsync = ({ dispatch }) => next => action => {
+	if (typeof action.value === 'function') {
+		dispatch({
+			type: action.type,
+			status: STATUS.PENDING
+		})
+		const promise = Promise
+			.resolve(action.value())
+			.then((value) => dispatch({
+				type: action.type,
+				status: STATUS.RESOLVED,
+				value
+			}))
+			.catch((error) => dispatch({
+				type: action.type,
+				status: STATUS.REJECTED,
+				error: error.message
+			}));
+		return promise;
+	}
+	return next(action);
+};
+
+const store = createStore(timeReducer, applyMiddleware(allowAsync));
+
+const {
+	setTime,
+	fetchTime,
+	fetchDate
+} = bindActionCreators(actions, store.dispatch);
+
+store.subscribe(() => {
+	console.log('x1b[1;34m%sxlb[0m', 'State has changed');
+	console.dir(
+		store.getState(),
+		{ colors: true, compact: false}
+	);
+});
+
+setTime(new Date().toTimeString());
+
+fetchTime();
+fetchDate();
+	
