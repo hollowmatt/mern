76610d12a3a228c4658de1e47833ed18625bd68b
Matt Holloway <hollowmatt@gmail.com>
2019-02-03 20:05:17 -0500

    Frame up the js file

---
+++ stateful-react.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class LifeCycleTime extends React.component {
+
+};
+
+class App extends React.component {
+
+};
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('[role="main"]')
+);
