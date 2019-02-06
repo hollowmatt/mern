f695dac7bede88c532578ccbdc82b10287b287ce
Matt Holloway <hollowmatt@gmail.com>
2019-02-05 20:13:48 -0500

    update index.html, create events.js

---
+++ events.js
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			title: 'Untitled'
+		}
+		this.onBtnClick = this.onBtnClick.bind(this);
+	}
+
+	onBtnClick() {
+		this.setState({
+			title: 'Hello there!'
+		});
+	}
+
+	render() {
+		return(
+			<section>
+				<h1>{this.state.title}</h1>
+				<button onClick={this.onBtnClick}>
+					Click to change title
+				</button>
+			</section>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('[role="main"]')
+);
